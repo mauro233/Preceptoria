556c4dca3d46936ff34d089bce4ed529959207a1
Mauro Ruiz <maurocas11@gmail.com>
2019-10-25 16:27:51 -0300

    Acomodando el login y la navegacion por la pagina
    Modifique el base.html para que se vea mas bonito y tambien para
    que si el usuario logueado pueda ver mas cosas que alguien que
    no lo este

--- Preceptoria/urls.py
+++ Preceptoria/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 
 urlpatterns = [
 	path('admin/', admin.site.urls),
-    path('', include('apps.alumno.urls')),
-    path('preceptoria/', include('apps.novedades.urls')),
+    path('alumno/', include('apps.alumno.urls')),
+    path('', include('apps.novedades.urls')),
     path('horario/', include('apps.horario.urls')),
 ]
--- apps/alumno/urls.py
+++ apps/alumno/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import SignUpView, BienvenidaView, SignInView, SignOutView
+from .views import SignUpView, SignInView, SignOutView
 
+app_name='sesion'
 urlpatterns = [
-    path('', BienvenidaView.as_view(), name='bienvenida'),
     path('registrate/', SignUpView.as_view(), name='sign_up'),
     path('incia-sesion/', SignInView.as_view(), name='sign_in'),
     path('cerrar-sesion/', SignOutView.as_view(), name='sign_out'),
