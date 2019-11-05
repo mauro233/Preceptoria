ad1d77d2019f00261696c592e7fadf7f107b3126
Mauro Ruiz <maurocas11@gmail.com>
2019-11-05 15:31:12 -0300

    Realizando cambios en el models de horarios_consulta y sus templates
    Agregamos mas campos q permitira que se especifiquen la hora de
    inicio y fin segun su respectivo dia

--- apps/horario/admin.py
+++ apps/horario/admin.py
@@ -10,5 +10,31 @@ class HorasInstitucionalesAdmin(admin.ModelAdmin):
 
 @admin.register(HorarioConsulta)
 class HorarioConsultaAdmin(admin.ModelAdmin):
-    list_display = ('profesor', 'materia', 'desde', 'hasta', 'horas_institucionales')
-    list_filter = ('profesor', 'materia', 'desde', 'hasta', 'horas_institucionales')
+    list_display = ('profesor', 'materia','horas_institucionales')
+    list_filter = ('profesor', 'materia', 'desde_lunes', 'hasta_lunes', 'desde_martes', 'hasta_martes', 'desde_miercoles', 'hasta_miercoles', 'desde_jueves', 'hasta_jueves', 'desde_viernes', 'hasta_viernes','horas_institucionales')
+    fieldsets = (
+    	(None, {
+    		'fields': ('profesor', 'materia', 'horas_institucionales')
+    	}
+    	),
+    	('Dia Lunes', {
+    		'fields': ('desde_lunes', 'hasta_lunes')
+        }
+        ),
+    	('Dia Martes', {
+    		'fields': ('desde_martes', 'hasta_martes')
+        }
+        ),
+    	('Dia Miercoles', {
+    		'fields': ('desde_miercoles', 'hasta_miercoles')
+        }
+        ),
+        ('Dia Jueves', {
+    		'fields': ('desde_jueves', 'hasta_jueves')
+        }
+        ),
+    	('Dia Viernes', {
+    		'fields': ('desde_viernes', 'hasta_viernes')
+        }
+        ),
+    )
