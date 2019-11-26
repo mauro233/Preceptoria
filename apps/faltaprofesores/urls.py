05e5632f8b60c6b7b7a8b38306656702d7174293
Mauro Ruiz <maurocas11@gmail.com>
2019-11-26 11:18:48 -0300

    Agregandole un motor de busqueda a la Falta de profesores
    Permite la busqueda de profesores, por materia y por fecha

--- apps/faltaprofesores/urls.py
+++ apps/faltaprofesores/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path, include
 from .views import(
-	ListFaltaProfesorView
+	ListFaltaProfesorView,
+	SearchResultFaltaProfesorView
 )
 
 app_name= 'falta_profesor'
 urlpatterns = [
     path('', ListFaltaProfesorView.as_view(), name='list'),
+    path('search/', SearchResultFaltaProfesorView.as_view(), name='search')
 ]
