428c69035636272a943f6dad13b4e88bbc9ba666
Mauro Ruiz <maurocas11@gmail.com>
2019-11-26 11:23:31 -0300

    Agregando el CRUD a las novedades
    Ahora tambien se permite borrar y actualizar una novedad

--- apps/novedades/urls.py
+++ apps/novedades/urls.py
@@ -3,15 +3,19 @@ from .views import(
 	ListAnuncioView,
 	DetailAnuncioView,
 	AnuncioList,
-	CreateAnuncioView
+	CreateAnuncioView,
+	DeleteAnuncioView,
+	UpdateAnuncioView
 )
 
 app_name= 'anuncios'
 urlpatterns = [
     path('serializers/', AnuncioList.as_view(), name='novedades'),
 	path('', ListAnuncioView.as_view(), name='list_anuncios'),
-	path('<int:pk>/', DetailAnuncioView.as_view(), name='detail_anuncio'),
-	path('crear/', CreateAnuncioView.as_view(), name='create_anuncio')
+	path('detalle/<int:pk>/', DetailAnuncioView.as_view(), name='detail_anuncio'),
+	path('crear/', CreateAnuncioView.as_view(), name='create_anuncio'),
+	path('borrar/<int:id>/', DeleteAnuncioView.as_view(), name='delete_anuncio'),
+	path('actualizar/<int:pk>/', UpdateAnuncioView.as_view(), name='update_anuncio')
 ]
 
 
--- apps/novedades/views.py
+++ apps/novedades/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
+from django.shortcuts import get_object_or_404
 from django.contrib.messages.views import SuccessMessageMixin 
 from django.urls import reverse_lazy
 from django.views.generic.edit import (
@@ -23,7 +24,7 @@ class ListAnuncioView(ListView):
 class DetailAnuncioView(DetailView):
 	model = Anuncio
 	template_name = 'novedades/detalle_anuncio.html'
-	context_object_name = 'detalle'
+	context_object_name = 'anuncio'
 
 
 class AnuncioList(generics.ListAPIView):
@@ -35,5 +36,22 @@ class CreateAnuncioView(SuccessMessageMixin, CreateView):
 	success_menssage = 'Anuncio Creado con Exito'
 	fields = ['titulo','descripcion','dirigido','tipo']
 	template_name = 'novedades/anuncio_create.html'
+	success_url = reverse_lazy('anuncios:list_anuncios') 
+
+class UpdateAnuncioView(SuccessMessageMixin, UpdateView):
+	model = Anuncio
+	success_menssage = 'Anuncio Actualizado con Exito'
+	fields = ['titulo','descripcion','dirigido','tipo']
+	template_name = 'novedades/anuncio_update.html'
 	success_url = reverse_lazy('anuncios:list_anuncios')  
+
+class DeleteAnuncioView(SuccessMessageMixin,DeleteView):
+	model = Anuncio
+	success_menssage='Anuncio Borrado con exito'
+	template_name = 'novedades/delete_anuncio.html'
+	success_url = reverse_lazy('anuncios:list_anuncios')
+
+	def get_object(self):
+		id_ = self.kwargs.get("id")
+		return get_object_or_404(Anuncio, id=id_)
 
